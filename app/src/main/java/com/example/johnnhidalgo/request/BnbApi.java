a8d05e4a46df563c041f0a081d6f237908499266
JohnnHidalgo <johnna.7h@gmail.com>
2020-12-06 10:26:06 -0400

    Make Api service for Element

--- app/src/main/java/com/example/johnnhidalgo/request/BnbApi.java
+++ app/src/main/java/com/example/johnnhidalgo/request/BnbApi.java
@@ -1,4 +1,20 @@
 package com.example.johnnhidalgo.request;
 
+import com.example.johnnhidalgo.request.responces.ElementResponce;
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface BnbApi {
+
+
+
+
+    @POST("/data")
+    Call<ElementResponce> getElement(
+           @Query("Title") String Title,
+           @Query("Description") String Description
+
+    );
+
 }
