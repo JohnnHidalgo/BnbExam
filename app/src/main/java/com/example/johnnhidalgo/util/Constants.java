c4813c23a90399f84468b7036a405691d2259e39
JohnnHidalgo <johnna.7h@gmail.com>
2020-12-06 10:11:08 -0400

    Create constant class

---
+++ app/src/main/java/com/example/johnnhidalgo/util/Constants.java
@@ -0,0 +1,7 @@
+package com.example.johnnhidalgo.util;
+
+public class Constants {
+    public static final String BASE_URL = "http://test.bnb.com.bo/mobiletestws/api/Values";
+    public static final String API_KEY = "";
+    public static final int NETWORK_TIMEOUT = 3000;
+}
